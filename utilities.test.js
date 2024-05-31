3981a039fb5c99d91603c1fb7791378afd13592c
TedMartell <martellted@gmail.com>
2024-05-31 22:39:12 +0300

    get inventory and remove from inventory added + tests

--- utilities.test.js
+++ utilities.test.js
@@ -1,10 +1,10 @@
 
 import { test, expect } from "@jest/globals";
 import { miningItems } from "./data/miningItems.js";
-import { addToInventory, removeFromInventory } from './utilities/inventory.js'
+import { addToInventory, getInventory, globalInventory, removeFromInventory} from './utilities/inventory.js'
 
   test('add Item to Inventory', () => {
-    const input = [miningItems.coal.name]
+    const input = [miningItems.coal]
     const actual = addToInventory(input)
     const expected = { Coal: 1 }
     expect(actual).toEqual(expected)
@@ -12,22 +12,57 @@ import { addToInventory, removeFromInventory } from './utilities/inventory.js'
 
   test('add multiple same Item to Inventory', () => {
     const input = 
-    [miningItems.coal.name, miningItems.coal.name]
+    [miningItems.coal, miningItems.coal]
     const actual = addToInventory(input)
     const expected = { Coal: 3 }
     expect(actual).toEqual(expected)
   })
 
   test('add multiple different Items to Inventory', () => {
-    const input = [miningItems.coal.name, miningItems.diamond.name, miningItems.zincOre.name]
+    const input = [miningItems.coal, miningItems.diamond, miningItems.zincOre]
     const actual = addToInventory(input)
     const expected = { "Coal": 4, "Diamond": 1, "Zinc Ore": 1 }
     expect(actual).toEqual(expected)
   })
 
-  test('remove Item', () => {
-    const input = [miningItems.coal.name]
-    const actual = removeFromInventory(input)
+  test('Get inventory, weight and value', () => {
+    const input = globalInventory
+    const actual = getInventory(input)
+    const expected = "Inventory: Coal: 4, Diamond: 1, Zinc Ore: 1\nTotal Weight: 8\nTotal Value: 59"
+    expect(actual).toEqual(expected)
+  })
+
+  test('remove Item from inventory', () => {
+    const input = [miningItems.coal]
+    const actual = removeFromInventory(input, 1)
     const expected = { "Coal": 3, "Diamond": 1, "Zinc Ore": 1 }
+    expect(actual).toEqual(expected)
+  })
+
+  test('remove multiple Items from inventory', () => {
+    const input = [miningItems.coal]
+    const actual = removeFromInventory(input, 3)
+    const expected = { "Diamond": 1, "Zinc Ore": 1 }
+    expect(actual).toEqual(expected)
+  })
+
+  test('Get inventory, weight and value', () => {
+    const input = globalInventory
+    const actual = getInventory(input)
+    const expected = "Inventory: Diamond: 1, Zinc Ore: 1\nTotal Weight: 4\nTotal Value: 55"
+    expect(actual).toEqual(expected)
+  })
+
+  test('remove none existing Item from inventory', () => {
+    const input = [miningItems.coal]
+    const actual = removeFromInventory(input, 1)
+    const expected = "Coal not found"
+    expect(actual).toEqual(expected)
+  })
+
+  test('remove none existing Item from inventory', () => {
+    const input = [miningItems.diamond]
+    const actual = removeFromInventory(input, 2)
+    const expected = "Not enough Diamond in inventory"
     expect(actual).toEqual(expected)
   })
--- utilities/inventory.js
+++ utilities/inventory.js
@@ -1,38 +1,69 @@
-export const globalInventory = {   
-}
+export const globalInventory = {}
+export let globalInventoryWeight = 0
+export let globalInventoryValue = 0
+export {addToInventory, getInventory, removeFromInventory}
 
 function addToInventory(items) {
     for (let item of items) {
-    if (!(item in globalInventory)) {
-        globalInventory[item] = 1
-    } else {
-        globalInventory[item] ++
-    }
-    console.log(`added ${item} to inventory`)   
-}
+    if (!(item.name in globalInventory)) {
+        globalInventory[item.name] = 1
 
+    } else {
+        globalInventory[item.name] ++
+    }
+    globalInventoryWeight += item.weight
+    globalInventoryValue += item.value
+}
 return globalInventory
 }
 
-function removeFromInventory(items) {
-    if (globalInventory.lenght === 0) {
-        return `Your inventory is empty!`
+function getInventory(globalInventory) {
+    let inventoryArray = Object.entries(globalInventory);
+    inventoryArray.sort((a, b) => b[1] - a[1]);
+
+    let inventoryString = '';
+    for (let [key, value] of inventoryArray) {
+        inventoryString += `${key}: ${value}, `;
     }
-    for (let item in items) {
-    if (!(item in globalInventory)) {
-        return `${item} not found`
-    } else {
-        globalInventory[item] --
-        console.log(`${item} removed from inventory`)
+
+    if (inventoryString.length > 0) {
+        inventoryString = inventoryString.slice(0, -2);
     }
+
+return  `Inventory: ${inventoryString}\nTotal Weight: ${globalInventoryWeight}\nTotal Value: ${globalInventoryValue}`;
+
 }
+
+
+
+function removeFromInventory(items, amount) {
+    if (Object.keys(globalInventory).length === 0) {
+        return `Your inventory is empty!`;
+    }
+
+    for (let item of items) {
+        if (!(item.name in globalInventory)) {
+            return `${item.name} not found`;
+        } else {
+            if (globalInventory[item.name] < amount) {
+                return `Not enough ${item.name} in inventory`;
+            }
+            globalInventory[item.name] -= amount;
+            globalInventoryWeight -= item.weight * amount;
+            globalInventoryValue -= item.value * amount;
+            
+            if (globalInventory[item.name] === 0) {
+                delete globalInventory[item.name];
+            }
+            
+            console.log(`${item.name} removed from inventory`);
+        }
+    }
 return globalInventory
 
 }
 
 
 
-export { 
-    addToInventory,
-    removeFromInventory,
-}
+
+
